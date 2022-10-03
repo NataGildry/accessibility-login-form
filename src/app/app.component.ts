7d39e6aa423d472c3f9aa06ef3231e6d1a0f3be4
NPodpriatova <natalia.podpryatova@gmail.com>
2022-10-03 13:47:30 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  onFormSubmit(userForm: NgForm): void {
+    console.log('done');
+  }
+}
